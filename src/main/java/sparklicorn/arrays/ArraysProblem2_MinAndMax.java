acf637627056cc7ec5e49c8608a52d6b23c23a10
Jeff <sparklicorn@users.noreply.github.com>
2019-04-20 00:54:07 -0500

    Add new arrays problem

---
+++ src/main/java/sparklicorn/arrays/ArraysProblem2_MinAndMax.java
@@ -0,0 +1,18 @@
+package sparklicorn.arrays;
+
+public class ArraysProblem2_MinAndMax {
+
+	/**
+	 * Finds the minimum and maximum values in the given list.
+	 * <br/>This method shall not mutate the given list.
+	 * @param list An array of integers.
+	 * @return An integer array containing two elements:
+	 * <code>{ minimumValue, maximumValue }</code>.
+	 * @throws IllegalArgumentException if the list is empty or null.
+	 */
+	public static int[] findMinAndMax(int[] list) {
+		//TODO
+
+		return null;
+	}
+}
