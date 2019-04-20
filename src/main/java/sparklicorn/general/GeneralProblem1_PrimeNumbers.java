ef09b32ea43c25aaacb3765b73c23270fe0d5739
Jeff <sparklicorn@users.noreply.github.com>
2019-04-20 12:32:17 -0500

    Add general problem 1 - prime numbers

---
+++ src/main/java/sparklicorn/general/GeneralProblem1_PrimeNumbers.java
@@ -0,0 +1,16 @@
+package sparklicorn.general;
+
+public class GeneralProblem1_PrimeNumbers {
+
+	/**
+	 * Determines if the given number is prime.
+	 * A prime number can only be divide evenly by 1 and itself.
+	 * @param n Number to check.
+	 * @return True if n is a prime number; otherwise false.
+	 */
+	public static boolean isPrime(int n) {
+		//TODO
+
+		return false;
+	}
+}
